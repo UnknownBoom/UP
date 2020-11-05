c90507692809d2c43729ded43f06cc664d143847
UnknownBoom <artem2001sarg@mail.ru>
2020-11-05 17:46:02 +0300

    init

---
+++ src/test/java/com/JSKP/KP/KpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JSKP.KP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
